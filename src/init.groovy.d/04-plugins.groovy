369326f2c9a15c92903a91fab5260deeabcf0eb6
Lucas Ko <lucasko.tw@gmail.com>
2022-04-05 17:38:35 +0800

    ok

--- src/init.groovy.d/04-plugins.groovy
+++ src/init.groovy.d/04-plugins.groovy
@@ -16,28 +16,31 @@ import hudson.plugins.sonar.model.TriggersConfig;
 
    def sonar_name = "MySonar"
    def sonar_server_url = "http://192.168.1.188:9000"
-   def sonar_auth_token = "my-sonar-token"
+   def sonar_credential_id = "my-sonar-id"
+//   def sonar_auth_token = "my-sonar-token"
+   def sonar_webhook_id = ''
    def sonar_mojo_version = ''
    def sonar_additional_properties = ''
-   def sonar_triggers = new TriggersConfig()
    def sonar_additional_analysis_properties = ''
+   def sonar_triggers = new TriggersConfig()
 
-   def new_inst = new SonarInstallation(
+    def new_inst = new hudson.plugins.sonar.SonarInstallation(
            sonar_name,
            sonar_server_url,
-           sonar_auth_token,
+           sonar_credential_id,
+           null, // Secret serverAuthenticationToken
+           sonar_webhook_id,
            sonar_mojo_version,
            sonar_additional_properties,
-           sonar_triggers,
-           sonar_additional_analysis_properties
+           sonar_additional_analysis_properties,
+           sonar_triggers
    )
    sonar_installations += new_inst
-   println("add new  sonar installation success :" + sonar_name);
+   println("add new sonar installation success :" + sonar_name);
 
    sonar_global_conf.setInstallations((SonarInstallation[]) sonar_installations)
    sonar_global_conf.save()
    jenkins_instance.save()
-
 
   }
  }
