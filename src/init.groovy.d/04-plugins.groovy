390eb4134b0412ac52a464deec9965a0eb95e980
Lucas Ko <lucasko.tw@gmail.com>
2022-04-05 18:35:04 +0800

    my-sonar-scanner

--- src/init.groovy.d/04-plugins.groovy
+++ src/init.groovy.d/04-plugins.groovy
@@ -1,6 +1,7 @@
 import jenkins.model.*
 import hudson.plugins.sonar.*
 import hudson.plugins.sonar.model.TriggersConfig;
+import hudson.tools.*
 
  class  MyPlugins {
   static void main(String... args) {
@@ -10,6 +11,9 @@ import hudson.plugins.sonar.model.TriggersConfig;
   }
 
   void setupSonar(){
+
+
+   // Step 1
    def jenkins_instance = Jenkins.getInstance()
    def sonar_global_conf = jenkins_instance.getDescriptor(SonarGlobalConfiguration.class)
    def sonar_installations = sonar_global_conf.getInstallations()
@@ -17,7 +21,6 @@ import hudson.plugins.sonar.model.TriggersConfig;
    def sonar_name = "MySonar"
    def sonar_server_url = "http://192.168.1.188:9000"
    def sonar_credential_id = "my-sonar-id"
-//   def sonar_auth_token = "my-sonar-token"
    def sonar_webhook_id = ''
    def sonar_mojo_version = ''
    def sonar_additional_properties = ''
@@ -40,6 +43,33 @@ import hudson.plugins.sonar.model.TriggersConfig;
 
    sonar_global_conf.setInstallations((SonarInstallation[]) sonar_installations)
    sonar_global_conf.save()
+   jenkins_instance.save()
+
+
+   def sonar_runner_name = "my-sonar-scanner"
+   def sonar_runner_version = "4.7.0.2747"
+   // Step 2 - Configure SonarRunner
+   println("Configuring SonarRunner...")
+   def desc_SonarRunnerInst = jenkins_instance.getDescriptor("hudson.plugins.sonar.SonarRunnerInstallation")
+   def sonarRunnerInstaller = new SonarRunnerInstaller(sonar_runner_version)
+   def installSourceProperty = new InstallSourceProperty([sonarRunnerInstaller])
+   def sonarRunner_inst = new SonarRunnerInstallation(sonar_runner_name + sonar_runner_version, "", [installSourceProperty])
+
+   def sonar_runner_installations = desc_SonarRunnerInst.getInstallations()
+   def sonar_runner_inst_exists = false
+   sonar_runner_installations.each {
+    installation = (SonarRunnerInstallation) it
+    if (sonarRunner_inst.getName() == installation.getName()) {
+     sonar_runner_inst_exists = true
+     println("Found existing installation: " + installation.getName())
+    }
+   }
+   if (!sonar_runner_inst_exists) {
+    sonar_runner_installations += sonarRunner_inst
+    desc_SonarRunnerInst.setInstallations((SonarRunnerInstallation[]) sonar_runner_installations)
+    desc_SonarRunnerInst.save()
+   }
+
    jenkins_instance.save()
 
   }
--- src/pipeline/TestJobs-pipeline.groovy
+++ src/pipeline/TestJobs-pipeline.groovy
@@ -1,9 +1,10 @@
+import hudson.plugins.sonar.SonarBuildWrapper
 environment{
 
 }
 
 pipeline {
-    agent any 
+    agent any
 
     stages {
 
@@ -23,6 +24,23 @@ pipeline {
             steps{
                 sh 'echo "./gradlew  test"'
             }
+        }
+
+        stage("sonar"){
+            steps{
+                script{
+                    sh "env"
+                    sh "java -version"
+                    def scannerHome = tool 'my-sonar-scanner';
+                    withSonarQubeEnv( envOnly: true) {
+                        println "${scannerHome}/bin/sonar-scanner"
+                        println "${env.SONAR_HOST_URL} "
+                        println "${env.SONAR_AUTH_TOKEN}"
+                        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=test-project"
+                    }
+                }
+            }
+
         }
 
     }//end of stages
