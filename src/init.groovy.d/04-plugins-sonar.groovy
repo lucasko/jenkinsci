6923d00d8a9219f0e977b299e359410643ae94a4
Lucas Ko <lucasko.tw@gmail.com>
2022-05-18 11:13:05 +0800

    4.3

--- src/init.groovy.d/04-plugins-sonar.groovy
+++ src/init.groovy.d/04-plugins-sonar.groovy
@@ -59,7 +59,7 @@ import hudson.tools.*
 
 
    def sonar_runner_name = "my-sonar-scanner"
-   def sonar_runner_version = "4.5.0.2216"
+   def sonar_runner_version = "4.3.0.2102"
    // Step 2 - Configure SonarRunner
    println("Configuring SonarRunner...")
    def desc_SonarRunnerInst = jenkins_instance.getDescriptor("hudson.plugins.sonar.SonarRunnerInstallation")
