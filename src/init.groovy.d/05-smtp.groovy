103f38c89ce248b567a53fb6bf971827edb7fb92
Lucas Ko <lucasko.tw@gmail.com>
2022-06-07 23:53:04 +0800

    disabled ssl

--- src/init.groovy.d/05-smtp.groovy
+++ src/init.groovy.d/05-smtp.groovy
@@ -1,5 +1,5 @@
 import jenkins.model.Jenkins
-import groovy.json.JsonSlurper.readJSON
+import groovy.json.JsonSlurper
 
 
 class  MySMTP {
@@ -10,8 +10,9 @@ class  MySMTP {
 
 
 //def envVars = Jenkins.instance.getGlobalNodeProperties()[0].getEnvVars()
+        def jsonSlurper = new JsonSlurper()
 
-        def props = readJSON file: '/code/secret.json'
+        def props = jsonSlurper.parse(new File('/code/secret.json'))
         print props
 
         def smtpUser = props['SMPT_USER']
@@ -23,7 +24,7 @@ class  MySMTP {
         emailExt.setSmtpAuth(smtpUser, smtpPassword)
         emailExt.setDefaultReplyTo(replyTo)
         emailExt.setSmtpServer(smtpServer)
-        emailExt.setUseSsl(true)
+//        emailExt.setUseSsl(true)
         emailExt.setSmtpPort(smtpPort)
         emailExt.setCharset("utf-8")
 //emailExt.setDefaultRecipients("someone@example.com")
@@ -31,3 +32,4 @@ class  MySMTP {
         emailExt.save()
 
     }
+}
